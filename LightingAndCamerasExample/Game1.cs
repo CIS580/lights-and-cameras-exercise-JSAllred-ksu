4984c1bb6faadf01c61b8a62e3949302773f7656
jsallred-ksu <jsallred@ksu.edu>
2024-11-06 16:38:44 -0600

    buh

--- LightingAndCamerasExample/Game1.cs
+++ LightingAndCamerasExample/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using static LightingAndCamerasExample.Crate;
 
 namespace LightingAndCamerasExample
 {
@@ -8,6 +9,9 @@ namespace LightingAndCamerasExample
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Crate[] crates;
+        //CirclingCamera camera;
+        FPSCamera camera;
 
         public Game1()
         {
@@ -28,6 +32,16 @@ namespace LightingAndCamerasExample
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            crates = new Crate[] {
+                new Crate(this, CrateType.DarkCross, Matrix.Identity),
+                new Crate(this, CrateType.Slats, Matrix.CreateTranslation(4, 0, 5)),
+                new Crate(this, CrateType.Cross, Matrix.CreateTranslation(-8, 0, 3)),
+                new Crate(this, CrateType.DarkCross, Matrix.CreateRotationY(MathHelper.PiOver4) * Matrix.CreateTranslation(1, 0, 7)),
+                new Crate(this, CrateType.Slats, Matrix.CreateTranslation(3, 0, -3)),
+                new Crate(this, CrateType.Cross, Matrix.CreateRotationY(3) * Matrix.CreateTranslation(3, 2, -3))
+            };
+            //camera = new CirclingCamera(this, new Vector3(0, 5, 10), 0.5f);
+            camera = new FPSCamera(this, new Vector3(0, 3, 10));
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,7 +50,7 @@ namespace LightingAndCamerasExample
                 Exit();
 
             // TODO: Add your update logic here
-
+            camera.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -45,7 +59,10 @@ namespace LightingAndCamerasExample
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            foreach (Crate crate in crates)
+            {
+                crate.Draw(camera);
+            }
             base.Draw(gameTime);
         }
     }
